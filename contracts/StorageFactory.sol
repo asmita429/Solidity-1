041934386ae8ec56423573b4f18c2b2651be6ab6
asmita429 <asmitathapa429@gmail.com>
2024-01-09 18:56:10 +0545

    deploying Simple-Storage contract from another contract  Storage-Factory

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+// either copy paste SimpleStorage.sol or import it. both will work 
+import"./SimpleStorage.sol";
+
+contract StorageFactory{
+    //creating a global variable of type SimpleStorage
+    // SimpleStorage public simplestorage; 
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();  //deploying SimpleStorage
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStorage(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        //Address
+        // ABI: Application Binary Interface
+
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.Store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.Retrieve();
+    }
+}

3d28d8c5b796da794d81c14b9495c4118cd5b493
asmita429 <asmitathapa429@gmail.com>
2024-01-10 17:14:37 +0545

    Fund transfers funds idk where, getprice eth=>usd with extra zeroes, version =>4 means take 4digits and then ' . '  ,getConversionRate gives eth=>usd

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,18 +3,49 @@
 // Set a min funding value in USD
 
 // SPDX-License-Identifier: MIT
-
 pragma solidity >=0.6.12 < 0.9.0;
 
+// import "./AggregatorV3Interface.sol";
+// or 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe{
-    
+
+    uint256 public minimumUSD = 50 * 1e18;
+
     function fund() public payable {
         // want to be able to set a minimun fund amount in USD
         // how can we send ETH to this contract?
 
-        require(msg.value > 1e18,"Didn't send enough!"); // 1e18 = 1* 10 ** 18 = 1000000000000000000
+        // require(msg.value > 1e18,"Didn't send enough!"); // 1e18 = 1* 10 ** 18 = 1000000000000000000
         //What is reverting? => undo any action before, and send remaining gas back
+
+        // require(msg.value > minimumUSD, "Didn't send enough");
+        require(getConversionRate(msg.value) >= minimumUSD, "didn't send enough");
     }
 
+    function getPrice() public view returns(uint256){
+        // ABI
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) = priceFeed.latestRoundData();
+
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 3000000
+        return uint256(price * 1e10); //1 ** 10 == 10000000000 
+    }
+
+    function getVersion() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint ethAmount) public view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount)/1e18;
+        return ethAmountInUsd;
+
+    }
     // function withdraw(){}
 }
